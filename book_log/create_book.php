41123c140d1d09724279c05cb86ad16ec0678d25
github yosshi-kataoka <github>
2023-07-12 21:43:16 +0900

    xss対策を実装。

--- book_log/create_book.php
+++ book_log/create_book.php
@@ -1,10 +1,11 @@
-<!--new_book.php/create_book.php-->
-<?php
-require_once __DIR__ . '../../companies/lib/mysqli.php';
+ <link rel="stylesheet" href="stylesheets/css/app.css">
+ <!--new_book.php/create_book.php-->
+ <?php
+  require_once __DIR__ . '/lib/mysqli.php';
 
-function createList($link, $book)
-{
-  $sql = <<<EOT
+  function createList($link, $book)
+  {
+    $sql = <<<EOT
 INSERT INTO book_log(
   title,
   author,
@@ -17,73 +18,76 @@ INSERT INTO book_log(
   "{$book['status']}",
   "{$book['score']}",
   "{$book['summary']}"
+  )
 EOT;
-  $result = mysqli_query($link, $sql);
-  if ($result) {
-  } else {
-    error_log('fail to create book_log') . PHP_EOL;
-    error_log('Debugging Error:' . mysqli_error($link)) . PHP_EOL;
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+    } else {
+      error_log('Error:fail to create book_log') . PHP_EOL;
+      error_log('Debugging Error:' . mysqli_error($link)) . PHP_EOL;
+    }
+    echo 'Error:データの追加に失敗しました' . PHP_EOL;
+    echo 'Debugging Error:' . mysqli_error($link) . PHP_EOL;
   }
-}
-
-//バリデーション処理
-function validate($book)
-{
-  $errors = [];
-  if (!strlen($book['title'])) {
-    $errors['title'] = '書籍名を入力してください';
-  } elseif (strlen($book['title']) > 100) {
-    $errors['title'] = '書籍名は100文字以内で入力してください';
-  }
-  if (!strlen($book['author'])) {
-    $errors['author'] = '著者名を入力してください';
-  } elseif (strlen($book['author']) > 100) {
-    $errors['author'] = '著者名は100文字以内で入力してください';
-  }
-  $statuses = ["unread", "reading", "complete"];
-  if (!in_array($book['status'], $statuses, true)) {
-    $errors['status'] = '未読、読んでる、読了より選択してください';
-  }
-  if (!(int)($book['score'])) {
-    $errors['score'] = '1以上5以下の整数を入力してください';
-  } elseif (1 > $book['score'] || 5 < $book['score']) {
-    $errors['score'] = '1以上5以下の整数を入力してください';
-  }
-  if (!strlen($book['summary'])) {
-    $errors['summary'] = '感想を入力してください';
-  } elseif (strlen($book['summary']) > 1000) {
-    $errors['summary'] = '感想は1000文字以内で入力してください';
-  }
-  return $errors;
-}
-//データを受け取り、変数へ格納
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $status = [];
-  if (array_key_exists('status', $_POST)) {
-    $status = $_POST['status'];
-  }
-  $book = [
-    'title' => $_POST['title'],
-    'author' => $_POST['author'],
-    'status' => $status,
-    'score' => $_POST['score'],
-    'summary' => $_POST['summary']
-  ];
 
   //バリデーション処理
-  $errors = validate($book);
-  if (!count($errors)) {
-    //データベース接続
-    $link = dbConnect();
-    //データベース追加
-    createList($link, $book);
-    //データベース解除
-    mysqli_close($link);
-    //リダイレクト
-    header("Location: index.php");
+  function validate($book)
+  {
+    $errors = [];
+    if (!strlen($book['title'])) {
+      $errors['title'] = '書籍名を入力してください';
+    } elseif (strlen($book['title']) > 100) {
+      $errors['title'] = '書籍名は100文字以内で入力してください';
+    }
+    if (!strlen($book['author'])) {
+      $errors['author'] = '著者名を入力してください';
+    } elseif (strlen($book['author']) > 100) {
+      $errors['author'] = '著者名は100文字以内で入力してください';
+    }
+    $statuses = ["unread", "reading", "complete"];
+    if (!in_array($book['status'], $statuses, true)) {
+      $errors['status'] = '未読、読んでる、読了より選択してください';
+    }
+    if (!(int)($book['score'])) {
+      $errors['score'] = '1以上5以下の整数を入力してください';
+    } elseif (1 > $book['score'] || 5 < $book['score']) {
+      $errors['score'] = '1以上5以下の整数を入力してください';
+    }
+    if (!strlen($book['summary'])) {
+      $errors['summary'] = '感想を入力してください';
+    } elseif (strlen($book['summary']) > 1000) {
+      $errors['summary'] = '感想は1000文字以内で入力してください';
+    }
+    return $errors;
   }
-}
-//もしエラーがあったら下記の処理を行う
-$title = '読書ログの登録';
-$content = __DIR__ . "/views/new_book.php";
-include 'views/layout.php';
+  //データを受け取り、変数へ格納
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $status = [];
+    if (array_key_exists('status', $_POST)) {
+      $status = $_POST['status'];
+    }
+    $book = [
+      'title' => $_POST['title'],
+      'author' => $_POST['author'],
+      'status' => $status,
+      'score' => $_POST['score'],
+      'summary' => $_POST['summary']
+    ];
+
+    //バリデーション処理
+    $errors = validate($book);
+    if (!count($errors)) {
+      //データベース接続
+      $link = dbConnect();
+      //データベース追加
+      createList($link, $book);
+      //データベース解除
+      mysqli_close($link);
+      //リダイレクト
+      header("Location: index.php");
+    }
+  }
+  //もしエラーがあったら下記の処理を行う
+  $title = '読書ログの登録';
+  $content = __DIR__ . "/views/new_book.php";
+  include 'views/layout.php';
--- book_log/index.php
+++ book_log/index.php
@@ -1,4 +1,23 @@
 <?php
+
+require_once __DIR__ . '/lib/escape.php';
+require_once __DIR__ . '../../companies/lib/mysqli.php';
+
+function ListBook($link)
+{
+  $books = [];
+  $sql = 'SELECT id,title,author,status,score,summary FROM book_log';
+  $results = mysqli_query($link, $sql);
+
+  while ($book = mysqli_fetch_assoc($results)) {
+    $books[] = $book;
+  }
+  mysqli_free_result($results);
+  return $books;
+}
+$link = dbConnect();
+$books = ListBook($link);
+
 $title = '読書ログの一覧';
 $content = __DIR__ . '/views/index.php';
 include __DIR__ . '/views/layout.php';
