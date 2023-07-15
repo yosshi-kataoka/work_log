428ead216a1c34ebd08837ea4056c09af6c26ce0
github yosshi-kataoka <github>
2023-07-15 23:52:41 +0900

    index.phpのsectionをデータベースから選択（料理名のみ）し、xss対策を実装。

--- cooking_log/views/index.php
+++ cooking_log/views/index.php
@@ -1,11 +1,9 @@
 <h1>料理のメモ</h1>
-<a href="#">料理の登録</a>
+<a class="text-decoration:none" href="../new.php">料理の登録</a>
 <h1>料理の一覧</h1>
-<section>
-  <h2>料理名</h2>&nbsp;|&nbsp;<span>和食</span>&nbsp;|&nbsp;<span>評価</span>
-  <p>レシピ</p>
-</section>
-<section>
-  <h2>料理名</h2>&nbsp;|&nbsp;<span>和食</span>&nbsp;|&nbsp;<span>評価</span>
-  <p>レシピ</p>
-</section>
+<?php foreach ($foods as $food) : ?>
+  <section class="card">
+    <h2>料理名:<?php echo escape($food['name']) ?></h2>&nbsp;|&nbsp;<span>和食</span>&nbsp;|&nbsp;<span>評価</span>
+    <p>レシピ</p>
+  </section>
+<?php endforeach ?>
