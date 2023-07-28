05cc0d2bdf1341457843ea06844825b53348d9f0
github yosshi-kataoka <github>
2023-07-28 15:29:05 +0900

    学んだ内容をもとにテレビの視聴時間Programを作ってみました。

--- quiz1.php
+++ quiz1.php
@@ -1,56 +1,57 @@
 <?php
-// $errors = [];
-// $totalTime = 0;
-// $chList = [
-//   'number' => '',
-//   'time' => '',
-//   'count' => ''
-// ];
-// $end = 0;
-// // 入力したch番号のエラー処理
-// function errorCheck($chList)
-// {
-//   if (!is_numeric($chList['number']) || $chList['number'] < 1 || $chList['number'] > 12) {
-//     echo "1から12までの整数を入力してください" . PHP_EOL;
-//     $errors['number'] = "1から12までの整数を入力してください";
-//   }
-//   if (!is_numeric($chList['time']) || 1 > $chList['time'] || 1440 < $chList['time']) {
-//     echo "1から1440までの数字を入力してください" . PHP_EOL;
-//     $errors['time'] = "1から1440までの数字を入力してください";
-//   }
-//   return $errors;
-// }
+// データ構造を決める
+// [[1]=>[min,min],[5]=>[min,min]...]
+// データの入力
+// データの取得
+// 望ましい形にデータを変換する
+// データの表示
 
-// // 実行内容
-// while ($end === 0) {
-//   // chの登録
-//   echo "1から12までのどのチャンネルを視聴しましたか" . PHP_EOL;
-//   $chList['number'] = trim(fgets(STDIN));
-//   // 視聴時間の登録
-//   echo "そのチャンネルを何分視聴しましたか" . PHP_EOL;
-//   $chList['time'] = trim(fgets(STDIN));
-//   // 視聴回数のカウント
-//   $chList['count']++;
-//   // バリデーション処理
-//   $errors = errorCheck($chList);
-//   var_dump($errors);
-//   if (count($errors) > 0) {
-//     echo $errors;
-//     continue;
-//   }
-//   $chLists[] = $chList;
-//   var_dump($chLists);
-//   // 結果を出力して終了
-//   $end = 1;
-// }
-// // echo $totalTime . EOL;
-// foreach($chList as$chList)
-
-
-
-// 回答
-function getInput()
+// 合計視聴時間(hour表示で、小数点第二位を切りすて)
+// チャンネル チャンネル合計視聴時間（min） チャンネル視聴回数
+// ...
+const sliceLength = 2;
+function getInput(): array
 {
-  var_dump(array_slice($_SERVER['argv'], 1));
+  $argument = array_slice($_SERVER['argv'], 1);
+  $input = array_chunk($argument, sliceLength);
+  return $input;
+}
+
+function parChannelTime(array $inputs): array
+{
+  $parChannelTimes = [];
+  foreach ($inputs as $input) {
+    $chan = $input[0];
+    $min = $input[1];
+    $mins = [$min];
+    if (array_key_exists($chan, $parChannelTimes)) {
+      $mins = array_merge($parChannelTimes[$chan], $mins);
+    }
+
+    $parChannelTimes[$chan] = $mins;
+  }
+  return $parChannelTimes;
+}
+
+function totalTime(array $parChannelTimes): float
+{
+  $parChannelTime = [];
+  foreach ($parChannelTimes as $time) {
+    $parChannelTime = array_merge($parChannelTime, $time);
+  }
+  $totalHour = array_sum($parChannelTime);
+  $totalTimes = round(($totalHour / 60), 1);
+  return $totalTimes;
+}
+
+function display($parChannelTimes): void
+{
+  $totalTimes = totalTime($parChannelTimes);
+  echo $totalTimes . PHP_EOL;
+  foreach ($parChannelTimes as $chan => $mins) {
+    echo $chan . ' ' . array_sum($mins) . ' ' . count($parChannelTimes[$chan]) . PHP_EOL;
+  }
 }
 $inputs = getInput();
+$parChannelTimes = parChannelTime($inputs);
+display($parChannelTimes);
