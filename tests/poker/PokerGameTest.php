87f5f11a81a0a83ddc6b7b5b744536d13103e9ab
github yosshi-kataoka <github>
2023-09-02 10:52:39 +0900

    VendingMachine.php Programの課題を解きました。

--- tests/poker/PokerGameTest.php
+++ tests/poker/PokerGameTest.php
@@ -6,9 +6,9 @@ require_once(__DIR__ . '/../../lib/poker/PokerGame.php');
 
 class PokerGameTest extends TestCase
 {
-  function testStart()
+  public function testStart()
   {
-    $game = new PokerGame(['CA', 'DA']);
-    $this->assertSame(['CA', 'DA'], $game->start());
+    $game = new PokerGame(['CA', 'DA'], ['C10', 'H10']);
+    $this->assertSame([['CA', 'DA'], ['C10', 'H10']], $game->start());
   }
 }
